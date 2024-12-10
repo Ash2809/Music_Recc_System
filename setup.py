8b421f153a9560b6b64a3e0afbafa725988c8cca
Ash2809 <aashutoshkumar2809@gmail.com>
2024-12-10 12:53:52 +0530

    requirments and project structure updated.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Music-Reccomdation-System',
+    version= '2.0.0',
+    author= 'Aashutosh Kumar and Qusai Madraswala',
+    author_email= 'aashutoshkumar2809@gmail.com, qmadrasa@gitam.in',
+    packages= find_packages(),
+    install_requires = []
+
+)
