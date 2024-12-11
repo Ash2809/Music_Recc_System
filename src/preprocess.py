1c982b77dc0c18970bf0199bb83355ea9b34dae2
Ash2809 <aashutoshkumar2809@gmail.com>
2024-12-11 10:18:21 +0530

    Data Preprocessing completed.

---
+++ src/preprocess.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+
+def preprocessor(path,output_path) -> pd.DataFrame:
+    dataset = pd.read_csv(path)
+
+    dataset=dataset.drop(columns=["Unnamed: 0", "track_id", "album_name", "track_name"], axis = 1)
+    dataset.dropna(inplace=True)
+    dataset["explicit"]=dataset["explicit"].astype(int)
+
+    le=LabelEncoder()
+    dataset["track_genre"]=le.fit_transform(dataset["track_genre"])
+    dataset["artists"]=le.fit_transform(dataset["artists"])
+
+    dataset.to_csv(output_path, header = True, index = False)
+    print(f"Preprocessed data saved to {output_path}")
+
+    return dataset
+
+if __name__ == "__main__":
+    path = r"C:\Projects\Music_Recc_System\data\dataset.csv"
+    output_path = r"C:\Projects\Music_Recc_System\data\processed.csv"
+    try:
+        dataset = preprocessor(path,output_path)
+        print("Data Preprocessing Completed.")
+    except Exception as e:
+        print("An error Occured while preprocessing {e}")
